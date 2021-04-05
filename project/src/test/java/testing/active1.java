650268b9e1930ce7f8b1ffb4c5ed27c01d597fd0
kghadi44 <60047559+kghadi44@users.noreply.github.com>
2021-04-05 19:57:23 +0530

    Add files via upload

---
+++ project/src/test/java/testing/active1.java
@@ -0,0 +1,30 @@
+package testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class active1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver_win32\\chromedriver.exe");
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("headless");
+		WebDriver driver=new ChromeDriver();
+		// Launch website  
+	    driver.navigate().to("http://192.168.43.251:49154/"); 
+	 // Click on the search text box and send value  
+	    driver.findElement(By.id("About Us")).click();
+	    WebElement TxtBoxContent = driver.findElement(By.id("PID-ab2-pg"));
+	    String Txt = TxtBoxContent.getText();
+	    System.out.println(Txt);
+	    driver.quit();
+	 
+		
+
+	}
+
+}
